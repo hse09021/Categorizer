ba246ec2c37c8b1317a00772a9f2ee3b5bfbe9f4
Mingyu Kim <51695816+hse09021@users.noreply.github.com>
2021-11-25 01:54:15 +0900

    Update mcu.ino

--- src/mcu/mcu.ino
+++ src/mcu/mcu.ino
@@ -1,16 +1,16 @@
 #include <Servo.h>
 
-Servo servo;
+Servo classifier;
 Servo lift;
 
 int data;
 
 void setup() {
   Serial.begin(9600);
-  servo.attach(9);
+  classifier.attach(9);
   lift.attach(8);
   lift.write(90);
-  servo.write(90);
+  classifier.write(90);
 }
 
 void loop() {
@@ -18,28 +18,28 @@ void loop() {
   while (Serial.available()) {
     data = Serial.read();
   
-  if (data == '0') {
-    servo.write(117);
-    delay(1000);
-    lift.write(40);
-    delay(6000);
-    servo.write(90);
-    delay(1000);
-    lift.write(90);
-    delay(1000);
-    
-  }
+    if (data == '0') {
+      classifier.write(117);
+      delay(1000);
+      lift.write(40);
+      delay(6000);
+      classifier.write(90);
+      delay(1000);
+      lift.write(90);
+      delay(1000);
 
-  else if (data == '1') {
-    servo.write(55);
-    delay(1000);
-    lift.write(40);
-    delay(6000);
-    servo.write(90);
-    delay(1000);
-    lift.write(90);
-    delay(1000);
-    
-  }
+    }
+
+    else if (data == '1') {
+      classifier.write(55);
+      delay(1000);
+      lift.write(40);
+      delay(6000);
+      classifier.write(90);
+      delay(1000);
+      lift.write(90);
+      delay(1000);
+
+    }
   }
 }
