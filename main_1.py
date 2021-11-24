69894038ba1370f8eed25176f71b21a5936a15c1
Joong-main <76873740+Joong-main@users.noreply.github.com>
2021-11-25 01:38:16 +0900

    Update main_1.py

--- main_1.py
+++ main_1.py
@@ -42,11 +42,17 @@ while True:
         break
 
 ############################################################################
-    # 7번 연속 판정 시키고 더 많이 판정된 결과(캔/병)를 디텍션 값으로 어싸인
+
 
     preprocessed=preprocessing(frame)
     prediciton = predict(preprocessed)
 
+    
+    #prediction[0,0] == Can
+    #prediction[0,1] == Bottle
+    #prediction[0,2] == Bg
+    
+    
     if (prediciton[0,0] <prediciton[0,2] and prediciton[0,1] <prediciton[0,2]):
         print("Bg")
     elif(prediciton[0, 0] < prediciton[0, 1] and prediciton[0, 2] < prediciton[0, 1]):
@@ -71,55 +77,4 @@ while True:
         time.sleep(9)
 
     cv2.imshow("VideoFrame",frame)
-#클래스 1이 else문
 
-
-
-
-
-
-
-
-
-'''
-2021/11/09
-while True:
-    ret, frame = capture.read()
-    if cv2.waitKey(100)>0:
-        break
-    preprocessed=preprocessing(frame)
-    prediciton = predict(preprocessed)
-## cnt 판별
-    if (prediciton[0,0] <prediciton[0,1]):
-        print("Bottle")
-        cv2.putText(frame,'Bottle',(0,25),cv2.FONT_HERSHEY_PLAIN,1,(0,0,0))
-    else:
-        cv2.putText(frame,"Can",(0,25),cv2.FONT_HERSHEY_PLAIN,1,(0,0,0))
-        print("Can")
-    cv2.imshow("VideoFrame",frame)
-#클래스 1이 else문
-'''
-
-
-
-'''
-# Create the array of the right shape to feed into the keras model
-# The 'length' or number of images you can put into the array is
-# determined by the first position in the shape tuple, in this case 1.
-data = np.ndarray(shape=(1, 224, 224, 3), dtype=np.float32)
-# Replace this with the path to your image
-image = Image.open('<IMAGE_PATH>')
-#resize the image to a 224x224 with the same strategy as in TM2:
-#resizing the image to be at least 224x224 and then cropping from the center
-size = (224, 224)
-image = ImageOps.fit(image, size, Image.ANTIALIAS)
-#turn the image into a numpy array
-image_array = np.asarray(image)
-# Normalize the image
-normalized_image_array = (image_array.astype(np.float32) / 127.0) - 1
-# Load the image into the array
-data[0] = normalized_image_array
-# run the inference
-prediction = model.predict(data)
-print(prediction)
-'''
